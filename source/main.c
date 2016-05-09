9d2196a4ce2a5ef5dca7575a1d184c684332113c
goncalo <goncalo.cool@gmail.com>
2016-05-09 21:29:47 +0100

    update ao main

--- source/main.c
+++ source/main.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 #include "hash.h"
 #include "avl.h"
 #include "auxiliares.h"
 
 #define MAXCHAR 140
 
-
-
 int main(){
 	bool le_hashtag = false;
-	int len_mensagem = 0, contador = 0;
+	int len_mensagem = 0, indice = 0, contador = 0, repeticoes = 0;
 	char c, palavra[MAXCHAR];
 	Item tag = (Item)malloc(sizeof(struct hashtags));
 	Item aux;
@@ -24,66 +23,79 @@ int main(){
 		switch (c){
 			case 'a':
 				c = getchar();
-				while(c = getchar())
+				while((c = getchar()) && len_mensagem < 140)
 				{
 					len_mensagem++;
 					
 					if(e_caracter_branco(c) && le_hashtag){
 						le_hashtag = false;
-						palavra[contador] = '\0';
-						contador = 0;
+						palavra[indice] = '\0';
+						indice = 0;
 						tag->hashtag = strdup(palavra);
 						tag->rep = 1;
 						aux = hashSearch(tag);
 
 						if (aux == NULLitem){
+							contador++;
+							repeticoes++;
 							tag->item = strdup(criaItem(tag->hashtag, tag->rep));
 							hashInsert(tag);
 						}
 						else{
 
 							aux->rep++;
+							repeticoes++;
 							aux->item = strdup(criaItem(aux->hashtag, aux->rep));
 						}
-
 					}
 					if(le_hashtag){
-						palavra[contador++] = c;
+						if (c >= 65 && c <= 90)
+							palavra[indice++] = tolower(c);
+						else
+							palavra[indice++] = c;
 					}
+
 					if (c == '#' && !(le_hashtag)){
 						le_hashtag = true;
-						palavra[contador++] = c;
+						palavra[indice++] = c;
 					}
 				}
 				
 				if (le_hashtag){
 					le_hashtag = false;
-					palavra[contador] = '\0';
-					contador = 0;
+					palavra[indice] = '\0';
+					indice = 0;
 					tag->hashtag = strdup(palavra);
 					tag->rep = 1;
 					aux = hashSearch(tag);
 
 					if (aux == NULLitem){
+						contador++;
+						repeticoes++;
 						tag->item = strdup(criaItem(tag->hashtag, tag->rep));
 						hashInsert(tag);
 					}
 					else{
 
 						aux->rep++;
+						repeticoes++;
 						aux->item = strdup(criaItem(aux->hashtag, aux->rep));
 					}
 				}
 				break;
+
 			case 's':
-				c = getchar();
+				printf("%d %d", contador, repeticoes);
 				break;
+
 			case 'm':
-				c = getchar();
+				//printf("%s %d", popular->hashtag, popular->rep);
+				break;
+
+			case 'l':
 				break;
 		}
 		if (c == 'x'){
-			free(tag);
 		}
 	}
 	return 0;
