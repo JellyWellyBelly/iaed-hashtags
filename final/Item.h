a08ca14cdfe5211a8edf689c89544ef744055355
System Administrator <root@mvs-pro.local>
2016-05-13 11:53:55 +0100

    Mudanca de planos

---
+++ final/Item.h
@@ -0,0 +1,41 @@
+#ifndef _ITEM_
+#define _ITEM_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef char* Key;
+
+typedef struct hashtags{
+char* item; 
+int rep;
+char* hashtag;
+}* Item;
+
+typedef struct nodeH{
+	Item conteudo;
+	struct nodeH *next;
+}* linkH;
+
+
+#define NULLitem NULL
+#define key(a) (a == NULL ? "" : a->item)
+#define keyNull(a) (a == NULL ? "" : "")
+
+
+
+
+#define less(a,b) (strcmp(a,b)<0)
+#define greater(a,b) (strcmp(a,b)>0)
+#define eq(a,b) (strcmp(a,b)==0)
+#define exch(A, B) { Item t = A; A = B; B = t; }
+#define compexch(A, B) if (less(B, A)) exch(A, B)	
+
+Item newItem(char*item, char *hashtag,int rep);
+void deleteItem(Item a);
+void visitItem(Item a);
+
+
+#endif
+
+
---
+++ final/avl.h
@@ -0,0 +1,27 @@
+#ifndef _AVL_
+#define _AVL_
+#include <stdlib.h>
+#include <stdio.h>
+#include "Item.h"
+
+typedef struct STnode* link;
+struct STnode { Item item; link l, r; int height;};
+//static link head;
+link avl;
+
+void STinit(link*);
+int STcount(link);
+Item STsearch(link,Key);
+void STinsert(link*,Item);
+void STdelete(link*,Key);
+void STsort(link,void (*visit)(Item));
+void STfree(link*);
+void STinit(link*);
+link NEW(Item item, link l, link r);
+link AVLbalance(link h);
+link rotR(link h);
+link rotLR(link h);
+link rotRL(link h);
+int Balance(link h);
+link insertR(link h, Item item);
+#endif
